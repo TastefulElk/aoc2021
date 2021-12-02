f49094ca2f02905d942293a6b147b3d1882349da
Sebastian Bille <sebastianbille@outlook.com>
2021-12-02 17:26:01 +0100

    add utility module

---
+++ src/utility/file_test.py
@@ -0,0 +1,19 @@
+import unittest
+from ..utility.file import read_lines # not sure why the .. is needed here 🤔
+
+class TestUtility(unittest.TestCase):
+
+    def test_read_lines_from_file(self):
+        res = read_lines("src/utility/utility_test_input.txt")
+        self.assertEqual(len(res), 3)
+        self.assertEqual(res[0], "abc\n")
+
+    def test_read_lines_from_file_throwsIfNonExistingFile(self):
+        with self.assertRaises(FileNotFoundError):
+            read_lines("src/utility/utility_test_input_non_existing.txt")
+
+        with self.assertRaises(FileNotFoundError):
+            read_lines("")
+
+if __name__ == '__main__':
+    unittest.main()
