106dc4c41b371633325eabc20ff29f4f2949cf8e
Sebastian Bille <sebastianbille@outlook.com>
2021-12-09 12:08:36 +0100

    add day 9

---
+++ tests/days/test_day_09.py
@@ -0,0 +1,19 @@
+import unittest
+from src.days.day_09.main import solve
+
+
+class TestUtility(unittest.TestCase):
+
+    def test_solve_part1(self):
+        actual = solve(1, True)
+        expected = 15
+        self.assertEqual(expected, actual)
+
+    def test_solve_part2(self):
+        actual = solve(2, True)
+        expected = 1134
+        self.assertEqual(expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
