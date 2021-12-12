3bfc8f0a4f8ea5d5bf6fa9033036a8b55e8ec2de
Sebastian Bille <sebastianbille@outlook.com>
2021-12-12 10:19:51 +0100

    add day 12

---
+++ tests/days/test_day_12.py
@@ -0,0 +1,19 @@
+import unittest
+from src.days.day_12.main import solve
+
+
+class TestUtility(unittest.TestCase):
+
+    def test_solve_part1(self):
+        actual = solve(1, True)
+        expected = 226
+        self.assertEqual(expected, actual)
+
+    def test_solve_part2(self):
+        actual = solve(2, True)
+        expected = 3509
+        self.assertEqual(expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
